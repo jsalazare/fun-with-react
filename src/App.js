f1803e950ccbc117fd7f6705d6035afa12086eb7
jsalazare <javi_9315@hotmail.com>
2020-05-15 12:35:37 -0700

    class component and JSX

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 import Greet from './components/Greet';
+import MyClass from './components/ClassComponent'
+import NoJsx from './components/NoJSX'
 
 function App() {
   return (
     <div className="App">
-      <Greet></Greet>
+      {/*<Greet></Greet>*/}
+      <MyClass></MyClass>
+      <NoJsx></NoJsx>
+
+
     </div>
   );
 }
