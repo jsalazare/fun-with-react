91dfef424661acc8511ef2262ed658eaa3ad8e89
jsalazar <jsalazar@skytouchtechnology.com>
2020-05-15 12:35:37 -0700

    class component and JSX

---
+++ src/components/ClassComponent.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class MyClassComponent extends Component {
+
+    render(){
+        return <h1>Hello dis is a class component</h1>
+    }
+        
+
+}
+
+export default MyClassComponent
---
+++ src/components/NoJSX.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+
+const NoJsx = () => 
+React.createElement('div',{id: 'myId'}, 
+    React.createElement('h1',null,'hi from no JSX functional component'));
+
+export default NoJsx
