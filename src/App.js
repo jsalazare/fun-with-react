ea11723fffd13c0cca7d6f241f3ca098873ece6b
jsalazar <jsalazar@skytouchtechnology.com>
2020-05-15 14:12:48 -0700

    state components

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 import Greet from './components/Greet';
 import MyClass from './components/ClassComponent'
 import NoJsx from './components/NoJSX'
+import MyElement from './components/MyElement';
+import ErrorBoundary from './components/ErrorBoundary';
+import StateComponent from './components/StateComponent';
 
 function App() {
   return (
     <div className="App">
       {/*<Greet></Greet>*/}
-      <MyClass></MyClass>
-      <NoJsx></NoJsx>
-
+      {/*<MyClass></MyClass>*/}
+      {/*<NoJsx></NoJsx>*/}
+      <StateComponent></StateComponent>
 
     </div>
   );
