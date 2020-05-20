f953e099426588f80daef87644ae348c381e5788
jsalazare <javi_9315@hotmail.com>
2020-05-19 20:25:20 -0700

    error handling missing app.js file

--- src/App.js
+++ src/App.js
@@ -9,25 +9,27 @@ import StateComponent from './components/StateComponent';
 import Counter from './components/Counter';
 import DestructuringStateAndProps from './components/DestructuringStateAndProps';
 import DestructuringPropsAndStateClassComponent
-  from './components/DestructuringPropsAndStateClassComponent';
+    from './components/DestructuringPropsAndStateClassComponent';
 import Events from './components/Events';
 
 function App() {
-  return (
-    <div className="App">
-      {/*<Greet></Greet>*/}
-      {/*<MyClass></MyClass>*/}
-      {/*<NoJsx></NoJsx>*/}
-      {/*<StateComponent></StateComponent>*/}
-      {/*<Counter></Counter>*/}
-      {/*<DestructuringStateAndProps name="juan" age="13" ></DestructuringStateAndProps>*/}
-      {/*<DestructuringPropsAndStateClassComponent name="juan" age="13" ></DestructuringPropsAndStateClassComponent>*/}
-      <Events></Events>
-
-
-
-    </div>
-  );
+    return (
+        <div className="App">
+            {/*<Greet></Greet>*/}
+            {/*<MyClass></MyClass>*/}
+            {/*<NoJsx></NoJsx>*/}
+            {/*<StateComponent></StateComponent>*/}
+            {/*<Counter></Counter>*/}
+            {/*<DestructuringStateAndProps name="juan" age="13" ></DestructuringStateAndProps>*/}
+            {/*<DestructuringPropsAndStateClassComponent name="juan" age="13" ></DestructuringPropsAndStateClassComponent>*/}
+            {/*================ Error Handling ==================*/}
+            <ErrorBoundary>
+              <MyElement name={'something Nice'}/>
+              <MyElement name={'ups not an error'}/>
+              <MyElement name={'error'}/>
+            </ErrorBoundary>
+        </div>
+    );
 }
 
 export default App;
